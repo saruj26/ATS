75fa3ae729fad709dd586c87170ac9133ad8b8bc
Sarujanan <sarujanang@gmail.com>
2025-12-13 19:35:32 +0530

    feat: Update CORS and CSRF settings to allow requests from Vercel domains

--- backend/settings.py
+++ backend/settings.py
@@ -128,9 +128,11 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://localhost:5173",
+    "https://*.vercel.app",
 ]
 CSRF_TRUSTED_ORIGINS = [
     "https://*.railway.app",
+    "https://*.vercel.app",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
