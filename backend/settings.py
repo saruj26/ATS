7023d49c5489a879f7483c024c942fc0cbaa09c9
Sarujanan <sarujanang@gmail.com>
2025-12-14 22:25:14 +0530

    fix: Normalize EMAIL_USE_TLS environment variable handling in settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -19,7 +19,7 @@ USE_SQLITE = os.getenv('USE_SQLITE', 'False') == 'True'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.gmail.com')
 EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "true").lower() == "true"
 EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER', 'sarujanang@gmail.com')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD', 'cofv vwry bmrr bknw')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
